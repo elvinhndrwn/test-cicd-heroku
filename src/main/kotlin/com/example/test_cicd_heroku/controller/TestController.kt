c3da5daf5335a2b99ef816611b68471a6281f2e5
elvinhndrwn <elvin.hendrawan@ist.id>
2024-09-17 10:52:56 +0700

    change response

--- src/main/kotlin/com/example/test_cicd_heroku/controller/TestController.kt
+++ src/main/kotlin/com/example/test_cicd_heroku/controller/TestController.kt
@@ -10,5 +10,5 @@ import org.springframework.web.bind.annotation.RestController
 class TestController {
 
     @GetMapping("/test")
-    fun test() : ResponseEntity<String> = ResponseEntity.ok("success build")
+    fun test() : ResponseEntity<String> = ResponseEntity.ok("success")
 }
